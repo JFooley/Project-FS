6b52f8c133003a3019d24002e10e2c7a3fce3789
João Gabriel <gabriel.lima.almeida@gmail.com>
2026-04-04 11:51:17 -0300

    Fix: Paths

--- Game/src/Language.cs
+++ Game/src/Language.cs
@@ -12,7 +12,7 @@ namespace Language_space {
         }
 
         private static void LoadAllLanguages() {
-            var languagesDir = "Assets/languages";
+            var languagesDir = Data.GetPath("Assets/languages");
             if (!Directory.Exists(languagesDir)) {
                 Console.WriteLine($"Diretório de idiomas não encontrado: {languagesDir}");
                 return;
