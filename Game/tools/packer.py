255544d35cee0c5725aa64b49c7d8dfd3c805d4a
JFooley <gabriel.lima.almeida@gmail.com>
2026-04-10 12:55:41 -0300

    Fix: folder names

--- Game/tools/packer.py
+++ Game/tools/packer.py
@@ -87,48 +87,48 @@ def main():
     ## Executar em Game pelo vs code
     
     # Visuals
-    pack_textures("Game/assets_raw/visuals", "Game/assets/visuals")
+    pack_textures("Game/raw/visuals", "Game/assets/visuals")
 
     # Stages
-    pack_textures("Game/assets_raw/stages/Burning Dojo/sprites", "Game/assets/stages/Burning Dojo")
-    pack_audio("Game/assets_raw/stages/Burning Dojo/sounds", "Game/assets/stages/Burning Dojo")
+    pack_textures("Game/raw/stages/Burning Dojo/sprites", "Game/assets/stages/Burning Dojo")
+    pack_audio("Game/raw/stages/Burning Dojo/sounds", "Game/assets/stages/Burning Dojo")
 
-    pack_textures("Game/assets_raw/stages/Japan Fields/sprites", "Game/assets/stages/Japan Fields")
-    pack_audio("Game/assets_raw/stages/Japan Fields/sounds", "Game/assets/stages/Japan Fields")
+    pack_textures("Game/raw/stages/Japan Fields/sprites", "Game/assets/stages/Japan Fields")
+    pack_audio("Game/raw/stages/Japan Fields/sounds", "Game/assets/stages/Japan Fields")
 
-    pack_textures("Game/assets_raw/stages/Night Alley/sprites", "Game/assets/stages/Night Alley")
-    pack_audio("Game/assets_raw/stages/Night Alley/sounds", "Game/assets/stages/Night Alley")
+    pack_textures("Game/raw/stages/Night Alley/sprites", "Game/assets/stages/Night Alley")
+    pack_audio("Game/raw/stages/Night Alley/sounds", "Game/assets/stages/Night Alley")
 
-    pack_textures("Game/assets_raw/stages/NYC Subway/sprites", "Game/assets/stages/NYC Subway")
-    pack_audio("Game/assets_raw/stages/NYC Subway/sounds", "Game/assets/stages/NYC Subway")
+    pack_textures("Game/raw/stages/NYC Subway/sprites", "Game/assets/stages/NYC Subway")
+    pack_audio("Game/raw/stages/NYC Subway/sounds", "Game/assets/stages/NYC Subway")
 
-    pack_textures("Game/assets_raw/stages/Rindo-Kan Dojo/sprites", "Game/assets/stages/Rindo-Kan Dojo")
-    pack_audio("Game/assets_raw/stages/Rindo-Kan Dojo/sounds", "Game/assets/stages/Rindo-Kan Dojo")
+    pack_textures("Game/raw/stages/Rindo-Kan Dojo/sprites", "Game/assets/stages/Rindo-Kan Dojo")
+    pack_audio("Game/raw/stages/Rindo-Kan Dojo/sounds", "Game/assets/stages/Rindo-Kan Dojo")
 
-    pack_textures("Game/assets_raw/stages/The Midnight Duel/sprites", "Game/assets/stages/The Midnight Duel")
-    pack_audio("Game/assets_raw/stages/The Midnight Duel/sounds", "Game/assets/stages/The Midnight Duel")
+    pack_textures("Game/raw/stages/The Midnight Duel/sprites", "Game/assets/stages/The Midnight Duel")
+    pack_audio("Game/raw/stages/The Midnight Duel/sounds", "Game/assets/stages/The Midnight Duel")
 
-    pack_textures("Game/assets_raw/stages/The Savana/sprites", "Game/assets/stages/The Savana")
-    pack_audio("Game/assets_raw/stages/The Savana/sounds", "Game/assets/stages/The Savana")
+    pack_textures("Game/raw/stages/The Savana/sprites", "Game/assets/stages/The Savana")
+    pack_audio("Game/raw/stages/The Savana/sounds", "Game/assets/stages/The Savana")
 
-    pack_textures("Game/assets_raw/stages/Training Stage/sprites", "Game/assets/stages/Training Stage")
-    pack_audio("Game/assets_raw/stages/Training Stage/sounds", "Game/assets/stages/Training Stage")
+    pack_textures("Game/raw/stages/Training Stage/sprites", "Game/assets/stages/Training Stage")
+    pack_audio("Game/raw/stages/Training Stage/sounds", "Game/assets/stages/Training Stage")
 
     # Particles
-    pack_textures("Game/assets_raw/particles/Hitspark/sprites", "Game/assets/particles/Hitspark")
-    pack_audio("Game/assets_raw/particles/Hitspark/sounds", "Game/assets/particles/Hitspark")
+    pack_textures("Game/raw/particles/Hitspark/sprites", "Game/assets/particles/Hitspark")
+    pack_audio("Game/raw/particles/Hitspark/sounds", "Game/assets/particles/Hitspark")
 
-    pack_textures("Game/assets_raw/particles/Particle/sprites", "Game/assets/particles/Particle")
-    pack_audio("Game/assets_raw/particles/Particle/sounds", "Game/assets/particles/Particle")
+    pack_textures("Game/raw/particles/Particle/sprites", "Game/assets/particles/Particle")
+    pack_audio("Game/raw/particles/Particle/sounds", "Game/assets/particles/Particle")
 
     # Fonts
-    pack_textures("Game/assets_raw/fonts", "Game/assets/fonts")
+    pack_textures("Game/raw/fonts", "Game/assets/fonts")
 
     # Characters
-    pack_textures("Game/assets_raw/characters/Ken/sprites", "Game/assets/characters/Ken")
-    pack_audio("Game/assets_raw/characters/Ken/sounds", "Game/assets/characters/Ken")
+    pack_textures("Game/raw/characters/Ken/sprites", "Game/assets/characters/Ken")
+    pack_audio("Game/raw/characters/Ken/sounds", "Game/assets/characters/Ken")
 
-    pack_textures("Game/assets_raw/characters/Ken-Fireball/sprites", "Game/assets/characters/Ken-Fireball")
-    pack_audio("Game/assets_raw/characters/Ken-Fireball/sounds", "Game/assets/characters/Ken-Fireball")
+    pack_textures("Game/raw/characters/Ken-Fireball/sprites", "Game/assets/characters/Ken-Fireball")
+    pack_audio("Game/raw/characters/Ken-Fireball/sounds", "Game/assets/characters/Ken-Fireball")
 
 main()
