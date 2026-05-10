10787e05c0c3fd2bb9528a7b428f18d981d7ac9d
JFooley <gabriel.lima.almeida@gmail.com>
2026-05-10 05:50:13 -0300

    Fix: stop tracking bin

--- Game/obj/Debug/net472/Project FS.AssemblyInfo.cs
+++ Game/obj/Debug/net472/Project FS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project FS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c98e575cf3fd665cb7c0025c0dfaceafe2f4b550")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+994f03fdabf20508b7c1fe47e3050202ae7fa953")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project FS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project FS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Game/obj/Release/net472/win-x64/Project FS.AssemblyInfo.cs
+++ Game/obj/Release/net472/win-x64/Project FS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project FS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d1c7e6ec7ac49ca78a09de9d249963d306470e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+994f03fdabf20508b7c1fe47e3050202ae7fa953")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project FS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project FS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
