64d134922f0adb7f2a1079ccd5f363b27b0dab2b
JFooley <gabriel.lima.almeida@gmail.com>
2026-07-10 08:06:28 -0300

    a

--- Game/src/Random.cs
+++ Game/src/Random.cs
@@ -1,7 +1,7 @@
-public class Random {
+public class Rand {
     private int seed = 0;
     public int Next(int min, int max) {
         seed = 1664525 * seed + (int) UI.frame_counter + 1013904223;
-        return (int)(seed % (uint)(max - min + 1)) + min;
+        return (int)(seed % (uint)(max - min)) + min; 
     }
 }
