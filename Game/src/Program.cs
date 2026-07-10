319d1cf4803d146a8990b7c8167e4285cc36be88
JFooley <gabriel.lima.almeida@gmail.com>
2026-07-09 23:13:18 -0300

    Feat: send/reciver screen

--- Game/src/Program.cs
+++ Game/src/Program.cs
@@ -32,6 +32,7 @@ public static class Program {
     // Session infos
     public static int playerA_wins = 0;
     public static int playerB_wins = 0;
+    public static int online_type = 0;
     
     // View
     public static View view = new View(new FloatRect(new Vector2f(0, 0), new Vector2f(Config.RenderWidth, Config.RenderHeight)));
--- Game/src/screens/IntroScreen.cs
+++ Game/src/screens/IntroScreen.cs
@@ -4,6 +4,8 @@ using SFML.System;
 
 public class WGIntro : Widget {
     private int pointer = 0;
+    private bool loading = false;
+    private Selector selector = new Selector(new List<int> {2, 1});
     Sprite fslogo;
 
     public WGIntro() {
@@ -11,16 +13,34 @@ public class WGIntro : Widget {
     }
 
     public override void Render() {
-        fslogo.Position = new Vector2f(10, 139);
-        Program.window.Draw(fslogo);
+        if (!loading) {
+            selector.Update();
 
-        if (UI.frame_counter % 20 == 0) pointer = pointer < 3 ? pointer + 1 : 0;
-        UI.DrawText(S(string.Concat(Enumerable.Repeat(".", pointer))), -122, 68, alignment: "left", spacing: -24);
+            if (UI.DrawButton(S("RECIVER"), 0, 0, action: Input.Key_up("A"), click: Input.Key_down("A"), hover: selector.is_on(0, 0), alignment: "right")) {
+                loading = true;
+                Program.online_type = 2;
+            }
+            if (UI.DrawButton(S("SENDER"), 0, 0, action: Input.Key_up("A"), click: Input.Key_down("A"), hover: selector.is_on(1, 0), alignment: "left")) {
+                loading = true;
+                Program.online_type = 1;
+            }
+            if (UI.DrawButton(S("OFFLINE"), 0, 15, action: Input.Key_up("A"), click: Input.Key_down("A"), hover: selector.is_on(0, 1), alignment: "center")) {
+                loading = true;
+                Program.online_type = 0;
+            }
 
-        if (!Program.loading) {
-            Thread main_loader = new Thread(Program.MainLoader);
-            main_loader.Start();
-            Program.loading = true;
+        } else {
+            fslogo.Position = new Vector2f(10, 139);
+            Program.window.Draw(fslogo);
+
+            if (UI.frame_counter % 20 == 0) pointer = pointer < 3 ? pointer + 1 : 0;
+            UI.DrawText(S(string.Concat(Enumerable.Repeat(".", pointer))), -122, 68, alignment: "left", spacing: -24);
+
+            if (!Program.loading) {
+                Thread main_loader = new Thread(Program.MainLoader);
+                main_loader.Start();
+                Program.loading = true;
+            }
         }
     }
 }
